cef981eec9c27e2110ce31d943d3093f4962ff7c
ivan.kiselev <ivan.kiselev@stm-labs.ru>
2022-03-10 10:45:59 +0300

    Task #3. Add API controller file

---
+++ PassportRecognitionProject/PassportRecognitionProject/Controllers/PassportRecognitionController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PassportRecognitionProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PassportRecognitionController : ControllerBase
+    {
+
+        private readonly ILogger<PassportRecognitionController> _logger;
+
+        public PassportRecognitionController(ILogger<PassportRecognitionController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
